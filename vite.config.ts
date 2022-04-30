e43ea8f82bbeeb3b5ad565c58b559b804659c08a
Dastan21 <loicd21@gmail.com>
2022-04-30 11:44:24 +0200

    Added port to vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 1102
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL("./src", import.meta.url))
